7a0a4ea5ade43eb283bfed8975f8676770b031b7
NoelGamaliel <techmbala@gmail.com>
2023-10-04 21:44:02 +0600

    AJout the new file appoitement and new content for the section my expertise

---
+++ appoitements.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- All link for style css -->
+    <link rel="stylesheet" href="./public/assets/style/style.css">
+    <link rel="stylesheet" href="./public/assets/style/@media.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <link rel="stylesheet" href="/animate.css/animate.min.css">
+
+
+    <title>Appointements</title>
+</head>
+<body>
+    <div class="container">
+        <section>
+            <h2>Your Welcome to Appointements</h2>
+            <div class="card-contact">
+                <form action="" method="post">
+                    <label for="">Name : </label>
+                    <input type="text" name="" id="">
+                </form>
+            </div>  
+        </section>
+    </div>
+</body>
+</html>
